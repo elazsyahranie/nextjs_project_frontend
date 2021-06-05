7ae6205157308235cad29d42a964846ff6ce2531
elazsyahranie <elazaribrahims@gmail.com>
2021-06-05 11:41:51 +0700

    Slicing home page

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,10 @@ import "../styles/globals.css";
 import "bootstrap/dist/css/bootstrap.css";
 import { ThemeProvider } from "styled-components";
 
+import "@fortawesome/fontawesome-svg-core/styles.css"; // import Font Awesome CSS
+import { config } from "@fortawesome/fontawesome-svg-core";
+config.autoAddCss = false; // Tell Font Awesome to skip adding the CSS automatically since it's being imported above
+
 function MyApp({ Component, pageProps }) {
   return (
     <>
--- pages/index.js
+++ pages/index.js
@@ -39,9 +39,9 @@ export default function Home(props) {
           <Navbar />
         </div>
         <div className="container py-5">
-          <div className="row">
+          <div className="row justify-content-between">
             <div
-              className={`${styles.whiteBackground} col-lg-3 col-md-3 col-sm-3 col-xs-12`}
+              className={`${styles.whiteBackgroundWithBorderRadius} col-lg-3 col-md-3 col-sm-3 col-xs-12`}
             >
               <div className="row py-5 px-4 w-100">
                 <div>
@@ -65,6 +65,60 @@ export default function Home(props) {
                 <div>
                   <img src="/homeOrIndex/grid.png"></img>
                   <span className="ms-5">Profile</span>
+                </div>
+              </div>
+            </div>
+            <div className={`col-lg-8 col-md-8 col-sm-8 col-xs-12`}>
+              <div className={`${styles.blueBackgroundWithBorderRadius}`}>
+                <div className="row justify-content-between p-4">
+                  <div
+                    className={`col-lg-5 col-md-5 col-sm-5 col-xs-5 ${styles.whiteText}`}
+                  >
+                    <span className="d-block">Balance</span>
+                    <h2>Rp. 120,000</h2>
+                    <span className="d-block">+62 813-9387-7946</span>
+                  </div>
+                  <div
+                    className={`col-lg-3 col-md-3 col-sm-3 col-xs-3 d-flex align-content-between flex-wrap ${styles.whiteText} ${styles.whiteText}`}
+                  >
+                    <button
+                      type="submit"
+                      className={`btn ${styles.myTransparentButton} d-block`}
+                    >
+                      Transfer
+                    </button>
+                    <button
+                      type="submit"
+                      className={`btn ${styles.myTransparentButton} d-block`}
+                    >
+                      Top Up
+                    </button>
+                  </div>
+                </div>
+              </div>
+              <div className="row justify-content-between p-4">
+                <div className="col-lg-5 col-md-5 col-sm-5 col-xs-5">
+                  <div className={`${styles.whiteBackgroundWithBorderRadius}`}>
+                    <img src="/homeOrIndex/in2.png"></img>
+                    {/* <span className="downArrow"></span>
+                    <style jsx>{`
+                      .downArrow::before {
+                        content: "\f063";
+                        font-family: "Font Awesome 5 Free";
+                        font-weight: 900;
+                        font-size: 2rem;
+                        color: #21b145;
+                      }
+                    `}</style> */}
+                    {/* <FontAwesomeIcon
+                      icon={faArrowDown}
+                      color="black"
+                      size="1px"
+                    /> */}
+                  </div>
+                </div>
+                <div className="col-lg-5 col-md-5 col-sm-5 col-xs-5">
+                  <h3>Hello 2</h3>
                 </div>
               </div>
             </div>
